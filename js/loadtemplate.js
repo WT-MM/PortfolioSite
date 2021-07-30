86949ffd5d54c6e578eaf97a36ac9c8863f13d07
Wesley Maa <wesley.maa@gmail.com>
2021-07-30 10:17:55 -0700

    add structure for existing projects

--- js/loadtemplate.js
+++ js/loadtemplate.js
@@ -1,7 +1,7 @@
 $(function () {
     var includes = $('[data-include]')
     $.each(includes, function () {
-      var file = '../templates/' + $(this).data('include') + '.html'
+      var file = '/templates/' + $(this).data('include') + '.html'
       $(this).load(file)
     })
   })

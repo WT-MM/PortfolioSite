0cedc642ad712faef731270eb59c9ff6c424f836
WT-MM <wesley.maa@gmail.com>
2021-08-15 23:02:53 -0700

    add macos functionality to book generation

--- tools/createBooks.py
+++ tools/createBooks.py
@@ -1,29 +1,32 @@
 from glob import glob
 from bs4 import BeautifulSoup
 from PIL import Image
+import os
 
-def resizeImg(url):
-    im = Image.open(url)
-    im.resize((1600,2500))
-    im.save(url)
 
 imgCat = glob("../reading/covers/*/")
-catNames = [i.split("\\")[-2] for i in imgCat]
+
+if os.name == "posix":
+    catNames = [i.split("/")[-2] for i in imgCat]
+elif os.name == "nt":
+    catNames = [i.split("\\")[-2] for i in imgCat]
 
 for i in range(len(imgCat)):
     html = BeautifulSoup("<!DOCTYPE html><html><body></body></html>", 'html.parser')
     itr = 0
+
     for j in glob(imgCat[i] + "*"):
-        resizeImg(j)
         div = html.new_tag('div')
+        '''
         if itr==0:
             div['class'] = "carousel-item active"
         else:
             div['class'] = "carousel-item"
+        '''
         img = html.new_tag("img")
         img['src'] = j
-        #img['width'] = "100%"
-        #img['height'] = "100%"
+        img['width'] = "150px"
+        img['height'] = "auto"
         img['class'] = "book d-block mx-auto img-fluid" 
         div.append(img)
         html.find("body").append(div)

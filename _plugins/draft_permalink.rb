fe48e9522ab1d77924851ab1cf6d08de07dc2ef5
Wesley Maa <wesley.maa@gmail.com>
2026-02-13 00:34:38 -0500

    idk codex fix

--- _plugins/draft_permalink.rb
+++
@@ -1,9 +0,0 @@
-module Jekyll
-  Hooks.register :posts, :post_init do |post|
-    tags = post.data['tags'] || []
-    next unless tags.include?('draft')
-    next if post.data['permalink']
-
-    post.data['permalink'] = "/drafts/#{post.date.strftime('%Y/%m-%d')}/#{post.slug}/"
-  end
-end

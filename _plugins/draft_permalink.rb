2ce1aa09d9a4c9d682a13462863945cf0f2e4240
Wesley Maa <wesley.maa@gmail.com>
2026-02-13 00:20:50 -0500

    routing fix

--- _plugins/draft_permalink.rb
+++ _plugins/draft_permalink.rb
@@ -1,9 +1,9 @@
 module Jekyll
-  Hooks.register :posts, :pre_render do |post|
+  Hooks.register :posts, :post_init do |post|
     tags = post.data['tags'] || []
     next unless tags.include?('draft')
     next if post.data['permalink']
 
-    post.data['permalink'] = "/drafts/#{post.slug}/"
+    post.data['permalink'] = "/drafts/#{post.date.strftime('%Y/%m-%d')}/#{post.slug}/"
   end
 end
